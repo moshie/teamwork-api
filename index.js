32867e0e4fa053a2010194c357856a9618e43b8e
David Hewitt <hello@wp-unite.io>
2018-01-07 21:05:09 +0000

    Testing api wrapper

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const tw = require('./test/helpers')
+require('dotenv').config()
 
-tw.account()
+const tw = require('./src')()
+
+tw.projects.getActivity(308180)
     .then((result) => console.log(result))
     .catch((error) => console.error(error))
