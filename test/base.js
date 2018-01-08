c38f5ed2adbad8679aefeb018216394f0fce5d45
David Hewitt <hello@wp-unite.io>
2018-01-08 19:57:45 +0000

    Started testing

---
+++ test/base.js
@@ -0,0 +1,15 @@
+const tw = require('./helpers')
+var chai = require('chai')
+var chaiAsPromised = require('chai-as-promised')
+
+chai.use(chaiAsPromised)
+
+chai.should()
+
+describe('#Base', function () {
+
+    it('should get the account information', function () {
+        
+    })
+
+})
