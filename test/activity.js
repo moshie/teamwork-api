b6d2b74b4bc157db47f23917936ca2c070860730
David Hewitt <hello@wp-unite.io>
2018-01-14 11:06:16 +0000

    Added tests for activities on a project and deletion of activity requests

--- test/activity.js
+++ test/activity.js
@@ -9,6 +9,8 @@ chai.should()
 
 // Mocked Data
 const latestActivityJson = require('./mocked-responses/latestActivity')
+const projectActivityJson = require('./mocked-responses/projectActivity')
+const deleteActivityJson = require('./mocked-responses/deleteActivity')
 
 describe('#Activity', function () {
 
@@ -27,6 +29,33 @@ describe('#Activity', function () {
         ])
     })
 
-    
+    it('GET /projects/{project_id}/latestActivity.json', function () {
+        nock(host)
+            .get('/projects/12345/latestActivity.json')
+            .reply(200, projectActivityJson)
+
+        let promise = tw.projects.getActivity(12345)
+
+        return Promise.all([
+            promise.should.eventually.be.an('object'),
+            promise.should.eventually.have.property('STATUS', 'OK').that.is.a('string'),
+            promise.should.eventually.have.property('activity').that.is.an('array'),
+            promise.should.eventually.have.deep.property('activity', projectActivityJson.activity)
+        ])
+
+    })
+
+    it('/activity/{activity_id}.json', function () {
+        nock(host)
+            .delete('/activity/12345.json')
+            .reply(200, latestActivityJson)
+
+        let promise = tw.activity.delete(12345)
+
+        return Promise.all([
+            promise.should.eventually.be.an('object'),
+            promise.should.eventually.have.property('STATUS', 'OK').that.is.a('string')
+        ])
+    })
 
 })
